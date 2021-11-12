c1e6626cb27e039779ed584fb5708bdbddb3e7f9
arashi5 <908189982@qq.com>
2021-11-12 15:09:24 +0800

    commit

--- app/src/main/java/scut/carson_ho/check_net/MainActivity.java
+++ app/src/main/java/scut/carson_ho/check_net/MainActivity.java
@@ -4,20 +4,19 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
 
 public class MainActivity extends AppCompatActivity {
 
     NetWorkStateReceiver netWorkStateReceiver;
+    private AppCompatTextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        tvContent = (AppCompatTextView)findViewById(R.id.tv_content);
 
-    //在onResume()方法注册
-    @Override
-    protected void onResume() {
         if (netWorkStateReceiver == null) {
             netWorkStateReceiver = new NetWorkStateReceiver();
         }
@@ -25,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         registerReceiver(netWorkStateReceiver, filter);
         System.out.println("注册");
-        super.onResume();
+
+        netWorkStateReceiver.callback = new NetWorkStateReceiver.Callback() {
+            @Override
+            public void addText(String text) {
+                String prevContent = tvContent.getText().toString();
+                tvContent.setText(prevContent.isEmpty() ? text : prevContent + "\n" + text);
+            }
+        };
     }
 
-    //onPause()方法注销
     @Override
-    protected void onPause() {
+    protected void onDestroy() {
         unregisterReceiver(netWorkStateReceiver);
         System.out.println("注销");
-        super.onPause();
+        super.onDestroy();
     }
 }
--- app/src/main/java/scut/carson_ho/check_net/NetWorkStateReceiver.java
+++ app/src/main/java/scut/carson_ho/check_net/NetWorkStateReceiver.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
-import android.widget.Toast;
 
 /**
  * Created by Carson_Ho on 16/10/31.
  */
 public class NetWorkStateReceiver extends BroadcastReceiver {
+    public Callback callback;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
@@ -23,36 +24,47 @@ public class NetWorkStateReceiver extends BroadcastReceiver {
             ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
             //获取ConnectivityManager对象对应的NetworkInfo对象
+            assert connMgr != null;
+
             //获取WIFI连接的信息
             NetworkInfo wifiNetworkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
             //获取移动数据连接的信息
             NetworkInfo dataNetworkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-            if (wifiNetworkInfo.isConnected() && dataNetworkInfo.isConnected()) {
-                Toast.makeText(context, "WIFI已连接,移动数据已连接", Toast.LENGTH_SHORT).show();
-            } else if (wifiNetworkInfo.isConnected() && !dataNetworkInfo.isConnected()) {
-                Toast.makeText(context, "WIFI已连接,移动数据已断开", Toast.LENGTH_SHORT).show();
-            } else if (!wifiNetworkInfo.isConnected() && dataNetworkInfo.isConnected()) {
-                Toast.makeText(context, "WIFI已断开,移动数据已连接", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(context, "WIFI已断开,移动数据已断开", Toast.LENGTH_SHORT).show();
+            //获取有线网连接的信息
+            NetworkInfo etherNetworkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(etherNetworkInfo.isConnected() ? "以太网已连接" : "以太网未连接; ");
+            sb.append(wifiNetworkInfo.isConnected() ? "WIFI已连接" : "WIFI未连接; ");
+            sb.append(dataNetworkInfo.isConnected() ? "移动数据已连接" : "移动数据未连接");
+
+            if (callback != null) {
+                callback.addText(sb.toString());
             }
-        }else {
+        } else {
             //这里的就不写了，前面有写，大同小异
             System.out.println("API level 大于21");
             //获得ConnectivityManager对象
             ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
             //获取所有网络连接的信息
+            assert connMgr != null;
             Network[] networks = connMgr.getAllNetworks();
             //用于存放网络连接信息
             StringBuilder sb = new StringBuilder();
             //通过循环将网络信息逐个取出来
-            for (int i=0; i < networks.length; i++){
+            for (Network network : networks) {
                 //获取ConnectivityManager对象对应的NetworkInfo对象
-                NetworkInfo networkInfo = connMgr.getNetworkInfo(networks[i]);
-                sb.append(networkInfo.getTypeName() + " connect is " + networkInfo.isConnected());
+                NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+                sb.append(networkInfo.getTypeName()).append(" connect is ").append(networkInfo.isConnected());
             }
-            Toast.makeText(context, sb.toString(),Toast.LENGTH_SHORT).show();
+            if (callback != null) {
+                callback.addText(sb.toString());
+            }
         }
+    }
+
+    public interface Callback {
+        void addText(String text);
     }
 }
